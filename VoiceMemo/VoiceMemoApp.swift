5b5f61ac04923d5f1a91c1a6cf007a3206d02016
xngsoo <hanxngsoo@gmail.com>
2024-02-26 19:37:40 +0900

    Create Project

---
+++ VoiceMemo/VoiceMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceMemoApp.swift
+//  VoiceMemo
+//
+//  Created by Han on 2/26/24.
+//
+
+import SwiftUI
+
+@main
+struct VoiceMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
